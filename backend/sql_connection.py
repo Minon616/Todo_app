78d1e83807b7acc52e6b48df31667c05e375cbd4
Minon Perera <probodha616@gmail.com>
2025-06-19 11:59:05 +0530

    Fix: corrected DB environment variable usage

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,13 +1,13 @@
-import os
 import mysql.connector
+import os
 
 def get_connection():
     try:
         connection = mysql.connector.connect(
-            host=os.environ['localhost'],
-            user=os.environ['root'],
-            password=os.environ['Minon#616747'],
-            database=os.environ['todo']
+            host=os.environ.get("DB_HOST", "localhost"),
+            user=os.environ.get("DB_USER", "root"),
+            password=os.environ.get("DB_PASSWORD", "Minon#616747"),
+            database=os.environ.get("DB_NAME", "todo")
         )
         return connection
     except mysql.connector.Error as err:
